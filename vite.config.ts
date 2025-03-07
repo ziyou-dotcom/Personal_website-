41a19e250583b3169c144966f16b7fd2928344f0
ziyou-dotcom <1812682747@qq.com>
2025-03-08 01:35:49 +0800

    配置 GitHub Pages 部署

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/',
   plugins: [react()],
+  base: 'https://zhaoliang.fun/',
   server: {
     port: 3000,
   },
